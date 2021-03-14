2a7e5c5f3a553e6db9d75c20ff9f14d65ead9943
Andrii Vakarev <avakarev@gmail.com>
2021-03-14 01:25:26 +0100

    Release dotfiles-0.1.1

--- Formula/dotfiles.rb
+++ Formula/dotfiles.rb
@@ -3,28 +3,28 @@
 class Dotfiles < Formula
   desc 'CLI utility to manage dotfiles symlinks'
   homepage 'https://github.com/avakarev/dotfiles-cli'
-  version '0.1.0'
+  version '0.1.1'
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/avakarev/dotfiles-cli/releases/download/0.1.0/dotfiles_0.1.0_darwin_amd64.tar.gz"
-    sha256 "44f2ae55c64a608d0a472058f88c1b0d8b0bf3f4667a87b8e71cf300baa7232c"
+    url "https://github.com/avakarev/dotfiles-cli/releases/download/0.1.1/dotfiles_0.1.1_darwin_amd64.tar.gz"
+    sha256 "66a2d47dd1b3b5b20b2d09fb9f4a1b6077c2a07dc3957aac7c2a284d11016cf8"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/avakarev/dotfiles-cli/releases/download/0.1.0/dotfiles_0.1.0_darwin_arm64.tar.gz"
-    sha256 "5052d8f3510ad1fc49f28cb87453573536aeca4a0f13013f81b75ee103d8a034"
+    url "https://github.com/avakarev/dotfiles-cli/releases/download/0.1.1/dotfiles_0.1.1_darwin_arm64.tar.gz"
+    sha256 "cfc365cd4a04cb918fa0c13f4d8813f60b996c215ead275de099689603cbeefa"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/avakarev/dotfiles-cli/releases/download/0.1.0/dotfiles_0.1.0_linux_amd64.tar.gz"
-    sha256 "c12f6b7927ea91785a3413be908393b25749995f98a1d72a3aeec2dbf997d25f"
+    url "https://github.com/avakarev/dotfiles-cli/releases/download/0.1.1/dotfiles_0.1.1_linux_amd64.tar.gz"
+    sha256 "943104ad9864bde497c913b3f3fef64c3aba36859959505bff449290221dc64c"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/avakarev/dotfiles-cli/releases/download/0.1.0/dotfiles_0.1.0_linux_armv6.tar.gz"
-    sha256 "a8dab9e6e06bb869205f2012d6da5bed7358c591e4fd0f40468d5ed1d1f6aad3"
+    url "https://github.com/avakarev/dotfiles-cli/releases/download/0.1.1/dotfiles_0.1.1_linux_armv6.tar.gz"
+    sha256 "68f8aae6be06d2176b9597e0a6817975742a09810e760c59f1224a7c67a433ea"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/avakarev/dotfiles-cli/releases/download/0.1.0/dotfiles_0.1.0_linux_arm64.tar.gz"
-    sha256 "0466b2dba894071cf5ca47f8246dd57ba31b0d2a9582b68be83635b1edc84f95"
+    url "https://github.com/avakarev/dotfiles-cli/releases/download/0.1.1/dotfiles_0.1.1_linux_arm64.tar.gz"
+    sha256 "54491f452016bbe7bcabe240997bc03866dde9e1cd86a90342ffd790f312f233"
   end
 
   def install
