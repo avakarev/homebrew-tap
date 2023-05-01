5b83e5a7c55f6ac52fa6e6af4d0b1b617f8c6527
Andrii Vakarev <avakarev@gmail.com>
2023-05-01 02:27:42 +0200

    Release hashify-1.1.0

--- Formula/hashify.rb
+++ Formula/hashify.rb
@@ -3,27 +3,27 @@
 class Hashify < Formula
   desc 'CLI utility to calculate file checksums'
   homepage 'https://github.com/avakarev/hashify'
-  version '1.0.0'
+  version '1.1.0'
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/avakarev/hashify/releases/download/1.0.0/hashify_1.0.0_darwin_amd64.tar.gz"
-    sha256 "db3a2fc2b6ad11d1b26b54c7b830a889c93b6838007095ba4daa7296f84dcd35"
+    url "https://github.com/avakarev/hashify/releases/download/1.1.0/hashify_1.1.0_darwin_amd64.tar.gz"
+    sha256 "cf5019879c8e9404e3ea7fe318bcc6f6da15bbfba1a6f60792d65cbc9dfa9683"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/avakarev/hashify/releases/download/1.0.0/hashify_1.0.0_darwin_arm64.tar.gz"
-    sha256 "63ba78e3a12a712b9116f9f93d90e5a7979b413cd52ff2e98b10fd269b9aea33"
+    url "https://github.com/avakarev/hashify/releases/download/1.1.0/hashify_1.1.0_darwin_arm64.tar.gz"
+    sha256 "b5213aa7c23d3cc5cf969b3aa255edd8ae4855ce4ca45a9e8f75e94bacee272f"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/avakarev/hashify/releases/download/1.0.0/hashify_1.0.0_linux_amd64.tar.gz"
-    sha256 "f72cb9003d104ecd43bbde0e4279a515365aa5f64b69c18119e6055dd7f89145"
+    url "https://github.com/avakarev/hashify/releases/download/1.1.0/hashify_1.1.0_linux_amd64.tar.gz"
+    sha256 "8433dcf9ac26ed6e44216e22f69de59f56ac18a8c8da3721dcd20d4966ba1c09"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/avakarev/hashify/releases/download/1.0.0/hashify_1.0.0_linux_arm64.tar.gz"
-    sha256 "1071cba74271f6a046d75225461a2c3b512174d8f9576acb1be7d0b4ca2e6103"
+    url "https://github.com/avakarev/hashify/releases/download/1.1.0/hashify_1.1.0_linux_arm64.tar.gz"
+    sha256 "22adf57c174d90a6d4efb4a6c7ae6014d223f8b7104bdf960f615fca3e5c7d14"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/avakarev/hashify/releases/download/1.0.0/hashify_1.0.0_linux_armv6.tar.gz"
-    sha256 "b515ab329401968959f86ba048a0e55e44c2cb13898c13db367c6d7b098aaf72"
+    url "https://github.com/avakarev/hashify/releases/download/1.1.0/hashify_1.1.0_linux_armv6.tar.gz"
+    sha256 "43578bd6a9a9958a28a78cb380b1b548460172c39445bbf8c6e5e2d68bff2d08"
   end
 
   def install
